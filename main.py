fc44043273c1f4f5b38eb4ff7214b6ea9d2d7b2b
vvitez <vitezvictor@gmail.com>
2021-09-24 23:46:41 +0200

    lmao

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO
 
+async_mode = None
 app = Flask(__name__)
+socket_ = SocketIO(app, async_mode=async_mode)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+
+def index():
+    return render_template('index.html',
+                           sync_mode=socket_.async_mode)
+
 
 if __name__ == '__main__':
-    app.run()
+    socket_.run(app, debug=True)
