7f098def918337ec06597819a3330e5ebddcdb7d
vvitez <vitezvictor@gmail.com>
2021-09-25 09:11:56 +0200

    lmao

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 
 async_mode = None
 app = Flask(__name__)
@@ -16,6 +16,11 @@ def index():
 def blabla():
     return render_template('blabla.html', 
                             sync_mode=socket_.async_mode)
+
+@socket_.on('connect')
+def test_connect():
+    emit('after connect', {'data': 'Hafti Abi Babi Strassenstyle.'})
+
 
 if __name__ == '__main__':
     socket_.run(app, debug=True)
