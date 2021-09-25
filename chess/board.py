a8c4354e75f58b1141b82e863f84e7c8f7c90ed7
ftchann <yann_huynh@hotmail.ch>
2021-09-25 10:22:32 +0200

    chess engine

--- chess/board.py
+++ chess/board.py
@@ -19,6 +19,7 @@ class Board:
         #Pieces to place in
         self.pieces = []
         self.set_initial_state()
+        self.enpassent = None
     def set_initial_state(self):
         #0 is white
         self.board[0][0] = Rook(0)
@@ -43,14 +44,15 @@ class Board:
         for i in range(8):
             self.board[i][6] = Pawn(1)
     def __str__(self):
+        answer = ""
         board_transposed = [list(x) for x in zip(*self.board)]
         for i in range(8):
             for j in range(8):
                 if board_transposed[i][j] is None:
                     board_transposed[i][j] = "E"
         for i in board_transposed:
-            print('\t'.join(map(str, i)))
-
+            answer += ('\t'.join(map(str, i))) + "\n"
+        return answer
     def inbound(self, coord):
         x = coord[0]
         y = coord[1]
@@ -58,8 +60,10 @@ class Board:
             return True
         else:
             return False
-    def get_pice_moves(self, coord, move_set, piece, turn):
-        ''' Get all of the possible moves from a piece '''
+    def get_piece_moves(self, coord, turn):
+        ''' Get all of the possible moves from a piece
+            Only Normal normals, No castling enpassent.
+        '''
 
         x = coord[0]
         y = coord[1]
@@ -76,6 +80,9 @@ class Board:
                 if piece_at_new_pos is None or piece_at_new_pos.color != turn:
                     possible_moves.append((coord, newpos))
         def add_new_pos(newpos, possible_moves):
+            #Not inbound break instantly
+            if not self.inbound(newpos):
+                return True
             piece_at_new_pos = self.board[newpos[0]][newpos[1]]
             if piece_at_new_pos is None:
                 possible_moves.append((coord, newpos))
@@ -110,6 +117,62 @@ class Board:
                 tobreak = add_new_pos(newpos, possible_moves)
                 if tobreak:
                     break
-
-
+        if piece.diag:
+            #Left Down
+            for i in range(1,7):
+                newpos = (x-i, y-i)
+                tobreak = add_new_pos(newpos, possible_moves)
+                if tobreak:
+                    break
+            #Right Down
+            for i in range(1,7):
+                newpos = (x+i, y-i)
+                tobreak = add_new_pos(newpos, possible_moves)
+                if tobreak:
+                    break
+            #Left Up
+            for i in range(1,7):
+                newpos = (x-i, y+i)
+                tobreak = add_new_pos(newpos, possible_moves)
+                if tobreak:
+                    break
+            #Right up
+            for i in range(1,7):
+                newpos = (x+i, y+i)
+                tobreak = add_new_pos(newpos, possible_moves)
+                if tobreak:
+                    break
+        #Pawn
+        if type(piece) is Pawn:
+            if piece.color == 0:
+                #Move
+                if self.board[x][y+1] is None:
+                    possible_moves.append((coord, (x,y+1)))
+                    if not piece.moved and self.board[x][y+2] is None:
+                        possible_moves.append((coord, (x, y+2)))
+                #Hit
+                left_hit:Piece = self.board[x-1][y+1]
+                right_hit:Piece = self.board[x-1][y+1]
+                if right_hit is not None and right_hit.color != turn:
+                    possible_moves.append((coord, (x-1,y+1)))
+                if left_hit is not None and left_hit.color != turn:
+                    possible_moves.append((coord, (x+1,y+1)))
+    def get_all_moves(self, turn):
+        ''' Get all of the possible normal moves from a piece
+        '''
+        allmoves = []
+        for i in range(8):
+            for j in range(8):
+                coord = (i,j)
+                if self.is_piece(self, coord):
+                    allmoves += self.get_piece_moves(coord, turn)
+        return allmoves
+    def is_piece(self, coord):
+        x = coord[0]
+        y = coord[1]
+        if self.board[x][y] is not None:
+            return True
+        else:
+            return False
 b = Board()
+print(b)
