e0291fe8f9792af11245500bb77d8943dd880179
vvitez <vitezvictor@gmail.com>
2021-09-25 13:06:25 +0200

    fucking finally

--- main.py
+++ main.py
@@ -3,23 +3,21 @@ from flask_socketio import SocketIO, emit
 
 async_mode = None
 app = Flask(__name__)
-socket_ = SocketIO(app, async_mode=async_mode)
+socket_ = SocketIO(app)
 
 @app.route('/')
 
 def index():
-    return render_template('test.html',
-                            sync_mode=socket_.async_mode)
+    return render_template('test.html')
 
 
 @app.route('/blabla')
 def blabla():
-    return render_template('blabla.html', 
-                            sync_mode=socket_.async_mode)
+    return render_template('blabla.html')
 
 @socket_.on('connect')
 def test_connect():
-    emit('after connect', {'chat message': 'Hafti Abi Babi Strassenstyle.'})
+    emit('chat message','Hafti Abi Babi Strassenstyle.')
 
 
 if __name__ == '__main__':
